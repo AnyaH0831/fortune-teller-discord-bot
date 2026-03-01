1fb5d8f5cdbdc661a9fac70c098da5664c37b6cc
Bianca <130195861+bianca-8@users.noreply.github.com>
2026-03-01 12:58:47 -0500

    Update eightball.py

--- cogs/eightball.py
+++ cogs/eightball.py
@@ -14,12 +14,12 @@ class EightBall(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="8ball", description="Ask the magical all-knowing 8-ball a question.")
+    @app_commands.command(name="8ball", description="Ask the slightly-less magical less all-knowing 8-ball a question.")
     async def magic_8ball(self, interaction: discord.Interaction, question: str):
         answer = random.choice(responses)
         await interaction.response.send_message(f'**Question:** {question}\n**8ball:** {answer}')
 
-    @app_commands.command(name="ask", description="Ask the slightly-less magical less all-knowing fortune teller a question.")
+    @app_commands.command(name="ask", description="Ask the magical all-knowing fortune teller a question.")
     async def gemini_8ball(self, interaction: discord.Interaction, question: str):
         await interaction.response.defer()
         if not GEMINI_API_KEY:
